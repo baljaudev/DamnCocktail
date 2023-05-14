0b1c878df6f90f08c3106d9c879f1f9edc6c8950
Baljaudev <r.baljau@gmail.com>
2023-05-14 19:12:14 +0200

    Creados GET para API

--- app/src/main/java/com/damncocktail/util/APIRestServicesCocktail.java
+++ app/src/main/java/com/damncocktail/util/APIRestServicesCocktail.java
@@ -5,24 +5,40 @@ import com.damncocktail.apidata.Cocktail;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface APIRestServicesCocktail {
 
-    public static final String BASE_URL = "www.thecocktaildb.com/api/json/v1/1/search.php";
+    public static final String BASE_URL = "https://www.thecocktaildb.com/api/json/";
 
     public static final String CLAVE_KEY = "1";
 
-    @GET("{key}")
-    Call<Cocktail> obtenerCines(
-            @Path("key") String key);
+    @GET("v1/{key}/search.php")
+    Call<Cocktail> obtenerCocktailNombre(
+            @Path("key") String CLAVE_KEY,
+            @Query("s") String nombre);
+
+
+    @GET("v1/{key}/search.php")
+    Call<Cocktail> obtenerCocktailIngrediente(
+            @Path("key") String CLAVE_KEY,
+            @Query("i") String ingrediente);
+
+
+    @GET("v1/{key}/search.php")
+    Call<Cocktail> obtenerCocktailPrimeraLetra(
+            @Path("key") String CLAVE_KEY,
+            @Query("f") String letra);
+
+
+    @GET("v1/{key}/filter.php")
+    Call<Cocktail> obtenerCocktailAlcohol(
+            @Path("key") String CLAVE_KEY,
+            @Query("a") String alcohol);
+
+
+    @GET("v1/{key}/random.php")
+    Call<Cocktail> obtenerCocktailRandom(
+            @Path("key") String CLAVE_KEY);
+
 }
-
-
-/*    @GET("{key}/{latitud},{longitud}")
-    Call<WeatherRes> obtenerTiempo(
-            @Path("key") String key,
-            @Path("latitud") double latitud,
-            @Path("longitud") double longitud,
-            @Query("exclude") String exclude, // Va detrás de un '?param=' en la URL
-            @Query("lang") String lang); // Va detrás de un '&param=' en la URL
-*/
