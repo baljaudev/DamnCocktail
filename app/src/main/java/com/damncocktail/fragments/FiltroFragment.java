c0be166c44bf13f5f1cd0326d631904731dbda37
Carloscrj <carlosramosjimenez.crj@gmail.com>
2023-05-14 19:06:26 +0200

    xml filtro

--- app/src/main/java/com/damncocktail/fragments/FiltroFragment.java
+++ app/src/main/java/com/damncocktail/fragments/FiltroFragment.java
@@ -30,15 +30,6 @@ public class FiltroFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FiltroFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static FiltroFragment newInstance(String param1, String param2) {
         FiltroFragment fragment = new FiltroFragment();
         Bundle args = new Bundle();
