6e07c60e77ec5dd66edad96b91f3097fe5bcef46
Baljaudev <r.baljau@gmail.com>
2023-05-14 12:51:43 +0200

    Implementado MainActivity e InicioFragment

--- app/src/main/java/com/damncocktail/MainActivity.java
+++ app/src/main/java/com/damncocktail/MainActivity.java
@@ -1,14 +1,59 @@
 package com.damncocktail;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+import com.damncocktail.fragments.AleatorioFragment;
+import com.damncocktail.fragments.FiltroFragment;
+import com.damncocktail.fragments.InicioFragment;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ImageButton btnAleatorio;
+    ImageButton btnFiltro;
+    FragmentManager fm;
+    FragmentTransaction ft;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fm = getSupportFragmentManager();
+        ft = fm.beginTransaction();
+
+        ft.add(R.id.flContenedor, new InicioFragment());
+        ft.commit();
+
+        btnAleatorio = findViewById(R.id.btnAleatorio);
+        btnFiltro = findViewById(R.id.btnFiltro);
+
+        btnAleatorio.setOnClickListener(this);
+        btnFiltro.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btnAleatorio) cargarAleatorio();
+        else if (v.getId() == R.id.btnFiltro) cargarFiltro();
+    }
+
+    private void cargarAleatorio() {
+        ft = fm.beginTransaction();
+        ft.replace(R.id.flContenedor, new AleatorioFragment());
+        ft.addToBackStack(null);
+        ft.commit();
+    }
+
+    private void cargarFiltro() {
+        ft = fm.beginTransaction();
+        ft.replace(R.id.flContenedor, new FiltroFragment());
+        ft.addToBackStack(null);
+        ft.commit();
     }
 }
---
+++ app/src/main/java/com/damncocktail/fragments/InicioFragment.java
@@ -0,0 +1,22 @@
+package com.damncocktail.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.damncocktail.R;
+
+public class InicioFragment extends Fragment {
+
+    public InicioFragment() {}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_inicio, container, false);
+    }
+}
